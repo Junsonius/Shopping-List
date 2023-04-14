436bde7eeae43140d2d8839ed18d025028eab0ab
Junsonius <sandrotsjunior+github@gmail.com>
2023-04-14 19:57:43 -0300

    organização de arquivo js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,6 @@ function produto(item, qnt, unit) {
     this.comprado = false 
 }
 
-/* 
-    adicionar categorias de produtos (frutas, carne, massa, etc.) via banco de dados
-*/
 function adicionarItem (produto) {
     lista.push(produto)
 }
@@ -33,30 +30,6 @@ document.getElementById("lista-principal").appendChild(ul)
 
 }
 
-/*function criarNovoItem (){
-    const novoItemAdd = document.createElement("ul")
-    novoItemAdd.classList.add("item", "item-lista-fundo", "center")
-    const itemlist1 = document.createElement("li")
-    const novoCheckBox = document.createElement("input")
-    novoCheckBox.type = "checkbox"
-    novoCheckBox.name = "comprado"
-    itemlist1.appendChild(novoCheckBox)
-
-    const itemlist2 = document.createElement("li")
-    const novoItemNome = document.createElement("p")
-    itemlist2.appendChild(novoItemNome)
-
-    const itemlist3 = document.createElement("li")
-    itemlist3.classList.add = ("qnt")
-    const novoItemQnt = document.createElement("p")
-    itemlist3.appendChild(novoItemQnt)
-
-    const novoElemento = document.querySelector(".lista-principal");
-    novoElemento.appendChild(itemlist1, itemlist2, itemlist3)
-
-}
-*/
-
 
 let lista = document.getElementsByClassName("item");
 
@@ -68,15 +41,4 @@ document.getElementById("add-btn").addEventListener("click", () =>{
     criarNovoItem(novoItem)
 })
 
-
 console.log(lista)
-
-/* 
-    <ul class="item item-lista-fundo center">
-        <li><input type="checkbox" name="Comprado"></li>
-        <li></li>
-        <li class="qnt">quantidade:</li>
-                        
-    </ul> 
-                    
-*/
