b38034f5e5874cfa63dbacc709250a4403d9c13d
Junsonius <sandrotsjunior+github@gmail.com>
2023-04-14 19:54:59 -0300

    funcionalidade de inclusão de itens na lista adicionado

--- js/main.js
+++ js/main.js
@@ -1,15 +1,39 @@
-function produto(item, qnt) {
+function produto(item, qnt, unit) {
     this.item = item;
     this.qnt = qnt;
+    this.unit = unit
     this.comprado = false 
 }
 
-
+/* 
+    adicionar categorias de produtos (frutas, carne, massa, etc.) via banco de dados
+*/
 function adicionarItem (produto) {
     lista.push(produto)
 }
 
-function criarNovoItem (){
+function criarNovoItem (novoItem){
+    let ul = document.createElement('ul');
+
+    let nomeItem = novoItem.item
+    let qntItem = novoItem.qnt
+    let unitItem = novoItem.unit
+
+    ul.setAttribute('class', 'item item-lista-fundo center');
+    ul.innerHTML = `
+    <ul class="item item-lista-fundo center">
+    <li><input type="checkbox" name="comprado"></li>
+    <li>${nomeItem}</li>
+    <li class="qnt">quantidade: ${qntItem} ${unitItem}</li>
+
+    </ul> 
+    `;
+
+document.getElementById("lista-principal").appendChild(ul)
+
+}
+
+/*function criarNovoItem (){
     const novoItemAdd = document.createElement("ul")
     novoItemAdd.classList.add("item", "item-lista-fundo", "center")
     const itemlist1 = document.createElement("li")
@@ -31,22 +55,20 @@ function criarNovoItem (){
     novoElemento.appendChild(itemlist1, itemlist2, itemlist3)
 
 }
+*/
 
-let dados = []
 
 let lista = document.getElementsByClassName("item");
 
 document.getElementById("add-btn").addEventListener("click", () =>{
     const nomeItem = document.getElementById("add-item").value;
     const qntItem = document.getElementById("add-qnt").value;
-    const novoItem = new produto(nomeItem, qntItem);
-    dados.push(novoItem);
-    criarNovoItem()
+    const unitItem = document.getElementById("option").value;
+    const novoItem = new produto(nomeItem, qntItem, unitItem);
+    criarNovoItem(novoItem)
 })
 
 
-
-    
 console.log(lista)
 
 /* 
